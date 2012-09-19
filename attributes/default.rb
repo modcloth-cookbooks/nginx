70317568325e192b09c0cf307371087405d85ea1
Ben Marini <bmarini@gmail.com>
2012-09-19 14:22:08 -0700

    Add gzip_buffers to main config

--- attributes/default.rb
+++ attributes/default.rb
@@ -57,6 +57,7 @@ default['nginx']['gzip']              = "on"
 default['nginx']['gzip_http_version'] = "1.0"
 default['nginx']['gzip_comp_level']   = "2"
 default['nginx']['gzip_proxied']      = "any"
+default['nginx']['gzip_buffers']      = "32 4k"
 default['nginx']['gzip_types']        = [
   "text/plain",
   "text/html",
